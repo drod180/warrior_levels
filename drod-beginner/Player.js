94f533c6f31a34f5d91a09b4ae223339d4279696
Dan Rodriguez <719drod@gmail.com>
2018-05-14 16:33:17 -0400

    Level 4

---
+++ drod-beginner/Player.js
@@ -0,0 +1,30 @@
+class Player {
+
+  playTurn(warrior) {
+    if (this.shouldHeal()) {
+      warrior.rest();
+    } else {
+      this.attackMove(warrior);
+    }
+  }
+
+  attackMove(warrior) {
+    if (warrior.feel().isEmpty() || warrior.attack()) {
+      warrior.walk();
+    }
+  }
+
+  beingAttacked(warrior) {
+    let attacked = false;
+    if (typeof previousHealth == 'undefined') {
+      let previousHealth = 20;
+    }
+    attacked = previousHealth > warrior.health();
+    previousHealth = warrior.health();
+    return attacked;
+  }
+
+  shouldHeal(warrior) {
+    return (!this.beingAttacked() && warrior.health() < Player.startingHealth)
+  }
+}
